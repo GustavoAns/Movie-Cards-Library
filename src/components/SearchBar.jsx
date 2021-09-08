2123870400b7e7452d6f29765a96ab1a6aa5105b
GustavoAns <gugasato9@live.com>
2021-09-08 15:16:57 -0300

    Ex 2 e 3

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,32 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      searchText: '',
+    };
+    this.onSearchTextChange = this.onSearchTextChange.bind(this)
+  }
+
+  onSearchTextChange(event) {
+    console.log(event.target.value)
   }
 
   render() {
     // console.log(this.props)
+    const { searchText } = this.state;
     return (
-      <div>
-        
-      </div>
+      <form data-testid="search-bar-form">
+        <label data-testid="text-input-label" htmlFor="searchText">
+          Inclui o texto:
+          <input
+          value={ searchText }
+          type="text"
+          name="searchText"
+          id="searchText"
+          onChange={ this.onSearchTextChange }
+          />
+        </label>
+      </form>
     );
   }
 }
