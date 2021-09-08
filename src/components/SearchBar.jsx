ef5e6b9b6dac509e8a97349f9a7620a71d5b6c9b
GustavoAns <gugasato9@live.com>
2021-09-08 15:56:26 -0300

    Ex 5

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,14 +3,16 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       searchText: '',
       bookmarkedOnly:false,
+      selectedGenre: '',
     };
 
     this.onSearchTextChange = this.onSearchTextChange.bind(this);
     this.onBookmarkedChange = this.onBookmarkedChange.bind(this);
+    this.onSelectedGenreChange = this.onSelectedGenreChange.bind(this);
   }
 
   onSearchTextChange(event) {
@@ -29,9 +31,17 @@ class SearchBar extends Component {
     });
   }
 
+  onSelectedGenreChange(event) {
+    
+  }
+
   render() {
     // console.log(this.props)
-    const { searchText, bookmarkedOnly } = this.state;
+    const { searchText, bookmarkedOnly, selectedGenre } = this.state;
+    const options = [
+      { value: 'chocolate', label: 'Chocolate' },
+      { value: 'teste', label: 'Teste' }
+    ]
     return (
       <form data-testid="search-bar-form">
         <label data-testid="text-input-label" htmlFor="searchText">
@@ -55,6 +65,21 @@ class SearchBar extends Component {
           id="bookmarkedOnly"
           onChange={this.onBookmarkedChange}
           />
+        </label>
+        <label data-testid="select-input-label" htmlFor="selectedGenre">
+          Filtrar por gênero
+          <select
+          data-testid="select-input"
+          value={selectedGenre}
+          name="selectedGenre"
+          id="selectedGenre"
+          onChange={this.onSelectedGenreChange}
+          >
+            <option value="" data-testid="select-option">Todos</option>
+            <option value="action" data-testid="select-option">Ação</option>
+            <option value="comedy" data-testid="select-option">Comédia</option>
+            <option value="thriller" data-testid="select-option">Suspense</option>
+          </select>
         </label>
       </form>
     );
